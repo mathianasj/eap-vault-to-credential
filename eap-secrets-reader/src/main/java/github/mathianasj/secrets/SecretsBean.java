86b1a7a262c9d6c288ae13f381569e495037d72a
null <null>
2023-09-15 14:38:45 +0000

    remove uneccessary methods

--- eap-secrets-reader/src/main/java/github/mathianasj/secrets/SecretsBean.java
+++ eap-secrets-reader/src/main/java/github/mathianasj/secrets/SecretsBean.java
@@ -5,15 +5,7 @@ import javax.annotation.Resource;
 
 @Stateful
 public class SecretsBean {
-    @Resource(name="myEnvEntry")
-    private String password;
-
     public String getPassword() {
         return System.getProperty("super.secret");
-    }
-
-    public void setPassword(String password) {
-        System.out.println("setting password");
-        this.password = password;
     }
 }
